f06c2241517c48b3a659154c5d60965e00ff2432
yurij <mihov.yurij@gmail.com>
2026-05-19 17:04:29 +0300

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,7 +4,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Критическая ошибка: Переменные не найдены в .env.local');
+  throw new Error('Критическая ошибка: Переменные не найдены в .env');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
